d0c568e0a026f65fc09c0260df188da5f215d167
ngzhekai <ngzhekai@gmail.com>
2022-04-06 21:24:20 +0800

    renamed lorem to Order History

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="#">Change Password</a></li>
-                                <li><a class="dropdown-item" href="#">lorem</a></li>
+                                <li><a class="dropdown-item" href="#">Order History</a></li>
                                 <li>
                                     <hr class="dropdown-divider">
                                 </li>
