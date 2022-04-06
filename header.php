9ee86722f64952071a98534728c9dc4afc504fb1
ngzhekai <ngzhekai@gmail.com>
2022-04-06 21:16:52 +0800

    added header.php

---
+++ header.php
@@ -0,0 +1,50 @@
+<header>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <div class="container-lg">
+                <a class="navbar-brand h1 mb-0" href="#">
+                    <img src="images/Yum!.png" alt="Yum.png" width="ms-auto" height="45"
+                        class="d-inline-block align-text-center"> YUM! Cafe
+                </a>
+                <!-- End of .navbar-brand -->
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <!-- End of .navbar-toggler  -->
+
+                <div class="collapse navbar-collapse justify-content-end flex-row-reverse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="menu.html">Menu</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                1191303027
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Change Password</a></li>
+                                <li><a class="dropdown-item" href="#">lorem</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Sign out</a></li>
+                            </ul>
+                            <!-- End of .dropdown-menu  -->
+                        </li>
+                        <a href="#" class="btn btn-outline-primary navbar-btn" id="btn-signin">Sign In</a>
+                    </ul>
+                    <!-- End of .navbar-nav  -->
+                </div>
+                <!-- End of .collapse .navbar-collapse .flex-row-reverse  -->
+            </div>
+            <!-- End of .container-lg  -->
+        </nav>
+        <!-- End of .navbar .navbar-expand-lg .navbar-light .bg-light  -->
+    </header>
+    <!-- End of header tag -->
