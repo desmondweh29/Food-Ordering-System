a2e8d4ab0e601cf455ab63e5f7188f6ae7839f7a
ngzhekai <ngzhekai@gmail.com>
2022-04-08 03:39:26 +0800

    make changes to receipt.php by copying from receipt.html

--- receipt.php
+++ receipt.php
@@ -2,78 +2,89 @@
 include_once 'header.php';
 ?>
 
-<div class="container-lg" id="section-1">
+<div id="section-1" class="container-lg">
     <div class="card mb-3">
-        <img src="images/receipt-bg.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h4 class="text-center mt-3 mb-5 text-capitalize">Thank You! <br> Your Order has been received by the Kitchen.</h4>
-            <h5 class="card-title fw-bold fs-2 mb-5 text-center">Order Receipt</h5>
+        <div class="row g-0">
+            <div class="col-lg-5 col-md-6">
+                <img src="images/receipt-bg1.jpg" id="receipt-bg" class="img-fluid rounded-start" alt="bg.png">
+            </div>
+            <!-- End of .col-lg-5 col-md-6 -->
+            <div class="col-lg-7 col-md-6">
+                <div class="card-body">
+                    <h4 class="text-center mt-5 mb-5 text-capitalize">Thank You! <br> Your Order has been received
+                        by
+                        the Kitchen.</h4>
+                    <h5 class="card-title fw-bold fs-2 mb-5 text-center">Order Receipt</h5>
 
-            <table class="table table-borderless mx-auto w-auto fs-5">
-                <tbody>
-                    <tr>
-                        <th scope="col">Order ID</th>
-                        <td>:</td>
-                        <td>#12344</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <td>:</td>
-                        <td>John</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Contact Number</th>
-                        <td>:</td>
-                        <td>0123456789</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <td>:</td>
-                        <td>8 April 2022</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Time</th>
-                        <td>:</td>
-                        <td>12:45</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Food</th>
-                        <td>:</td>
-                        <td>Milo x1 <br> Mushroom Soup x1</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Price (RM)</th>
-                        <td>:</td>
-                        <td>69.90</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Type</th>
-                        <td>:</td>
-                        <td>Delivery</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Address1</th>
-                        <td>:</td>
-                        <td>Jalan Ayer Keroh Lama, </td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Address2</th>
-                        <td>:</td>
-                        <td>75450 Bukit Beruang, Melaka</td>
-                    </tr>
-                    <tr>
-                        <th scope="col">Remarks</th>
-                        <td>:</td>
-                        <td>Milo ice tak mau ice. <br> Extra sauce.</td>
-                    </tr>
-                </tbody>
-                <!-- End of tbody  -->
-            </table>
-            <!-- End of .table .table-borderless mx-auto w-auto  -->
+                    <table class="table table-borderless mx-auto w-auto fs-6">
+                        <tbody>
+                            <tr>
+                                <th scope="col">Order ID</th>
+                                <td>:</td>
+                                <td>#12344</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <td>:</td>
+                                <td>John</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Contact Number</th>
+                                <td>:</td>
+                                <td>0123456789</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Date</th>
+                                <td>:</td>
+                                <td>8 April 2022</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Time</th>
+                                <td>:</td>
+                                <td>12:45</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Food</th>
+                                <td>:</td>
+                                <td>Milo x1 <br> Mushroom Soup x1</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Price (RM)</th>
+                                <td>:</td>
+                                <td>69.90</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Type</th>
+                                <td>:</td>
+                                <td>Delivery</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Address1</th>
+                                <td>:</td>
+                                <td>Jalan Ayer Keroh Lama, </td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Address2</th>
+                                <td>:</td>
+                                <td>75450 Bukit Beruang, Melaka</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Remarks</th>
+                                <td>:</td>
+                                <td>Milo ice tak mau ice. <br> Extra sauce.</td>
+                            </tr>
+                        </tbody>
+                        <!-- End of tbody  -->
+                    </table>
+                    <!-- End of .table .table-borderless mx-auto w-auto  -->
+                </div>
+                <!-- End of .card-body  -->
+            </div>
+            <!-- End of .col-lg-7 .col-md-6  -->
         </div>
-        <!-- End of .card-body  -->
+        <!-- End of .row .g-0  -->
     </div>
-    <!-- End of .card .mb-5  -->
+    <!-- End of .card .mb-3  -->
 </div>
 <!-- End of #section-1.container-lg  -->
 
