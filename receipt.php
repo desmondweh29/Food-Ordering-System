b308164c66df12833b275bc9de2fdfe4d0d21652
ngzhekai <ngzhekai@gmail.com>
2022-04-08 02:29:35 +0800

    modified receipt.html and added receipt.php

---
+++ receipt.php
@@ -0,0 +1,82 @@
+<?php
+include_once 'header.php';
+?>
+
+<div class="container-lg" id="section-1">
+    <div class="card mb-3">
+        <img src="images/receipt-bg.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h4 class="text-center mt-3 mb-5 text-capitalize">Thank You! <br> Your Order has been received by the Kitchen.</h4>
+            <h5 class="card-title fw-bold fs-2 mb-5 text-center">Order Receipt</h5>
+
+            <table class="table table-borderless mx-auto w-auto fs-5">
+                <tbody>
+                    <tr>
+                        <th scope="col">Order ID</th>
+                        <td>:</td>
+                        <td>#12344</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <td>:</td>
+                        <td>John</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Contact Number</th>
+                        <td>:</td>
+                        <td>0123456789</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Date</th>
+                        <td>:</td>
+                        <td>8 April 2022</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Time</th>
+                        <td>:</td>
+                        <td>12:45</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Food</th>
+                        <td>:</td>
+                        <td>Milo x1 <br> Mushroom Soup x1</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Price (RM)</th>
+                        <td>:</td>
+                        <td>69.90</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Type</th>
+                        <td>:</td>
+                        <td>Delivery</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Address1</th>
+                        <td>:</td>
+                        <td>Jalan Ayer Keroh Lama, </td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Address2</th>
+                        <td>:</td>
+                        <td>75450 Bukit Beruang, Melaka</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Remarks</th>
+                        <td>:</td>
+                        <td>Milo ice tak mau ice. <br> Extra sauce.</td>
+                    </tr>
+                </tbody>
+                <!-- End of tbody  -->
+            </table>
+            <!-- End of .table .table-borderless mx-auto w-auto  -->
+        </div>
+        <!-- End of .card-body  -->
+    </div>
+    <!-- End of .card .mb-5  -->
+</div>
+<!-- End of #section-1.container-lg  -->
+
+<?php
+include_once 'footer.php';
+?>
