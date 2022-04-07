fd35eb7426065415062aeffc69652329a5bc98a8
ngzhekai <ngzhekai@gmail.com>
2022-04-07 20:29:09 +0800

    added a little spacing between the navbar and picture

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     include_once 'header.php';
 ?>
 
-    <div class="container-md" id="section-1">
+<div class="container-lg" id="section-1">
 
         <img src="images/food.jpg" alt="food.jpg" class="img-fluid mx-auto d-block">
         <div id="about-us">
@@ -13,10 +13,13 @@
                 mollitia optio dolorum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore impedit
                 eligendi,
                 veniam eaque laborum deserunt necessitatibus atque repudiandae expedita voluptate incidunt molestias
-                voluptatum sed libero perferendis corrupti? Expedita, corrupti itaque.</p>
+                voluptatum sed libero perferendis corrupti? Expedita, corrupti itaque.lorem Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Molestiae maiores quo non ut cumque dolorum culpa reprehenderit hic
+                placeat, repellat eligendi distinctio minus illo deleniti, quaerat perferendis alias itaque nisi!</p>
         </div>
         <!-- End of #about-us  -->
     </div>
+    <!-- End of #section-1 .container-md  -->
     
 <?php
     include_once 'footer.php';
