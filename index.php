e54574256e3b747b32455b14a8a30a024d40bc5c
ACh1cken <68433261+ACh1cken@users.noreply.github.com>
2022-04-08 01:37:11 +0800

    added about us on index

--- index.php
+++ index.php
@@ -7,15 +7,12 @@
         <img src="images/food.jpg" alt="food.jpg" class="img-fluid mx-auto d-block">
         <div id="about-us">
             <h2><br>About Our Cafe</h2>
-            <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam qui, ea dignissimos
-                dolores, suscipit
-                necessitatibus commodi excepturi maxime placeat numquam rem? Voluptates deleniti in neque ullam quaerat
-                mollitia optio dolorum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore impedit
-                eligendi,
-                veniam eaque laborum deserunt necessitatibus atque repudiandae expedita voluptate incidunt molestias
-                voluptatum sed libero perferendis corrupti? Expedita, corrupti itaque.lorem Lorem, ipsum dolor sit amet
-                consectetur adipisicing elit. Molestiae maiores quo non ut cumque dolorum culpa reprehenderit hic
-                placeat, repellat eligendi distinctio minus illo deleniti, quaerat perferendis alias itaque nisi!</p>
+            <p class="text-justify" style="font-family: cursive; ">
+            Here at Yum's, we aim to bring you a beautiful dining experience.
+            We have one simple vision. 
+            Give our customers great food and uncompromising service for breakfast and lunch dining.
+            Our menu consists of local as well as overseas delicacy. Sourcing for ingredients as natural and organic wherever possible, without compromising on deliciousness, so that you feel good about what you're eating.
+            We absolutely can't wait to share the love with you!</p>
         </div>
         <!-- End of #about-us  -->
     </div>
